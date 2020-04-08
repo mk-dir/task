c4bbdcb6dc4faeba328988b9cb7ad1319bb65d8b
mk-dir <48390573+mk-dir@users.noreply.github.com>
2020-04-08 17:59:43 +0300

    Update task.py
    Look at that, what can you infer?

--- task.py
+++ task.py
@@ -1,13 +1,17 @@
-'''listA=["a-b-c", "d-e-f", "g-h-i"]
-listb= ["a-b-c", "d-z-f", "g-h-i"]
-print(listb)'''
+listA = ['a-b-c', 'd-e-f', 'g-h-i']
 
+def convert(list1):
+    listB=[]
+    for each in list1:
+        counter=0
+        leach=list(each)
+        while counter < len(leach):
+            if leach[counter]=='e':
+                leach[counter]='z'
 
+            
+            counter+=1
+        listB.append(''.join(leach))
+    return listB
 
-def listA(a,b,c):
-    listA = ["a-b-c", "d-e-f", "g-h-i"]
-    return listb
-listb=["a-b-c", "d-z-f", "g-h-i"]
-print(listb)
-
-
+print(convert(listA))

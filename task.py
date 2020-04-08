9726d8c504f2021d545916edf6ce8cee2d89c7cc
Kerenyambura <kerenwangombe@gmail.com>
2020-04-08 16:16:39 +0300

    task

--- task.py
+++ task.py
@@ -1,3 +1,13 @@
-listA=["a-b-c", "d-e-f", "g-h-i"]
-listA[1]= "d-z-f"
-print(listA)
+'''listA=["a-b-c", "d-e-f", "g-h-i"]
+listb= ["a-b-c", "d-z-f", "g-h-i"]
+print(listb)'''
+
+
+
+def listA(a,b,c):
+    listA = ["a-b-c", "d-e-f", "g-h-i"]
+    return listb
+listb=["a-b-c", "d-z-f", "g-h-i"]
+print(listb)
+
+
